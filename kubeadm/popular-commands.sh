f730c97fa0d8fa9d02eb3d537fd188a756005436
DeewhyCodes <131259901+DeewhyCodes@users.noreply.github.com>
2024-05-02 11:36:28 +0100

    Update popular-commands.sh

--- kubeadm/popular-commands.sh
+++ kubeadm/popular-commands.sh
@@ -5,11 +5,16 @@ kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml: This comm
 kubectl get nodes: This command lists the nodes in the cluster.
 kubectl get pods: This command lists the pods in the default namespace.
 kubectl get pods --all-namespaces: This command lists the pods in all namespaces.
+kubectl get pods --show-labels: This command lists the pods in the specified namespace including their labels
 kubeadm token create --print-join-command: This command generates a join command for worker nodes to join the cluster.
 kubeadm token list: This lists all the tokens and their validity
 kubectl run nginx-demo --image=nginx --port=80: This command runs a new pod with the nginx image and exposes port 80.
 kubectl expose pod nginx-demo --port=80 --target-port=80 --type=NodePort: This command exposes the nginx-demo pod as a NodePort service.
 kubectl get services: This command lists the services in the default namespace.
+kubectl get svc/ep/namespace/pods -o wide: this lists any of the options with more deatils about them.
+kubectl describe  svc/ep/namespace/pods: this shows full information of any of the specified option
+kubectl describe  svc/ep/namespace/pods -ns(wherever applicable)/svc(wherever applicable)/po(where applicable) nameofOption: this lists information about the specified option.
+kubectl config set-context --current --namespace=my-namespace: this sets the default namespace to the one specified
 
 
 
