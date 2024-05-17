ad5acd9b20218e15f14115ee354f8864405adcbd
DeeFromNextDoor <simeonvictordayo@gmail.com>
2024-05-17 09:08:42 +0100

    completed first variables practice

---
+++ terraform/variables02/resource.tf
@@ -0,0 +1,21 @@
+# using terraform  high level block syntax, create an ec2-instance and use a userdata to set up and install apache,
+# also attach an elastic IP to the instance. Pass expressions and properties use variables
+
+#To make our work neat, we can create seperate files for the providers and variables
+
+#3) resource block, to create the instance
+resource "aws_instance" "test_ec2" {
+  ami           = var.my_ami #call the ami variable
+  instance_type = var.my_instance_type[0] #call the instance_type variable t2.micro by its index (0)
+  #instance_type = var.my_instance_type["dev"] #to call the instance_type from a map variable.
+
+  user_data = file("${path.module}/apache-install.sh")
+  tags = {
+    Name = var.my_instance_name
+  }
+}
+
+#4)resource block, to create an elastic IP resource
+resource "aws_eip" "my_eip" {
+  instance = aws_instance.test_ec2.id
+}
---
+++ terraform/variables02/variables.tf
@@ -0,0 +1,34 @@
+#Variables configuration file
+
+#declare region variable
+variable "us_region" {
+  type = string
+  default = "us-east-1" #if we don't enter a default value, then an input value would be requested during terraform plan
+}
+#declare ami variable
+variable "my_ami" {
+  type = string
+  default = "ami-04b70fa74e45c3917"
+}
+#declare instance_type variable
+variable "my_instance_type" {
+  type = list(string) #passing the variables as a list of strings 
+  default = ["t2.micro", "t2.medium", "t3.micro"] #the array of strings to be selected by their index
+}
+
+#Declare variable using tye (map)
+/*variable "my_instance_type" {
+  type = map(string)
+  default = {
+    "dev" = "t2.micro"
+    "staging" = "t2.medium"
+    "prod" = "t2.large"
+  }
+} */
+
+
+#declare instance_name
+variable "my_instance_name" {
+  type = string
+  default = "Ec2-Demo"
+}
